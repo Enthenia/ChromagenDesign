8ade6765dc865cde5edd01683bb7eeb21abc3009
Enthenia <ryoshan@protonmail.com>
2024-11-07 20:36:30 -0800

    Update invitation.js

--- js/invitation.js
+++ js/invitation.js
@@ -1,13 +1,19 @@
-//function created to replace the content
-function replaceContent() {
-	//Variable declaration
-	var myParticipantName;
-	
-	//setting the variable to the input field's id named participantNameInput's value
-	myParticipantName = document.getElementById("participantNameInput").value;
-	
-	console.log('Variable myParticipantName: ' + myParticipantName);
-	
-	//setting the HTML code in the span id ParticipantNamePlaceholder with the variable 
-	document.getElementById("participantNamePlaceholder").innerHTML = myParticipantName;
+/* The script to dynamically update the invitation */
+
+// This function is invoked when the page is fully loaded
+window.onload = init;
+
+function init() {
+    const submit = document.getElementById("submit");
+
+    // When the user clicks the submit button
+    submit.onclick = function () {
+        const name = document.getElementById("participantName").value;
+        const sponsername = document.getElementById("sponserName").value;
+
+        // Update the placeholders with user input
+        document.getElementById("participantNamePlaceholder").innerText = name;
+        document.getElementById("outputsponserNamePlaceholder").innerText = sponsername;
+    };
+}
 } 
